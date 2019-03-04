9e4ce56b37ed3b1da5ce183b3f21b8ae7cc1820d
noah-jonathan rosa <noah-jonathan.rosa-ext@st.com>
2019-03-04 11:13:54 +0100

    Fix warnings and add sam compatibility

--- examples/X_NUCLEO_53L0A1_HelloWorld/X_NUCLEO_53L0A1_HelloWorld.ino
+++ examples/X_NUCLEO_53L0A1_HelloWorld/X_NUCLEO_53L0A1_HelloWorld.ino
@@ -50,8 +50,24 @@
 #include <stdint.h>
 #include <assert.h>
 
+#ifdef ARDUINO_SAM_DUE
+#define DEV_I2C Wire1
+#elif defined(ARDUINO_ARCH_STM32)
 #define DEV_I2C Wire
+#else
+#define DEV_I2C Wire
+#endif
 #define SerialPort Serial
+
+//For SAM compatibility where D8 and D2 are undefined
+#ifndef D8
+#define D8 8
+#endif
+
+
+#ifndef D2
+#define D2 2
+#endif
 
 // Components.
 STMPE1600DigiOut *xshutdown_top;
@@ -70,6 +86,26 @@ void setup() {
 
   // Initialize serial for output.
   SerialPort.begin(115200);
+
+//NOTE: workaround in order to unblock the I2C bus on the Arduino Due
+#ifdef ARDUINO_SAM_DUE
+   pinMode(71, OUTPUT);
+   pinMode(70, OUTPUT);
+
+   for (int i = 0; i<10; i++){
+     digitalWrite(70, LOW);
+     delay(3);
+     digitalWrite(71, HIGH);
+     delay(3);
+     digitalWrite(70, HIGH);
+     delay(3);
+     digitalWrite(71, LOW);
+     delay(3);
+   }
+   pinMode(70, INPUT);
+   pinMode(71, INPUT);
+#endif
+//End of workaround
 
   // Initialize I2C bus.
   DEV_I2C.begin();
@@ -159,4 +195,3 @@ void loop() {
     SerialPort.println(report);
   }
 }
-
